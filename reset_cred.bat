ba72b111eddc0cd41bc2646366c5f36056502e3b
HopzAlot <rehansaqib2006@gmail.com>
2026-03-05 16:19:15 +0500

    Adding reset bad file.

---
+++ reset_cred.bat
@@ -0,0 +1,17 @@
+@echo off
+echo ================================================
+echo   BASHIR AUTCONNECT!! YOUR HOSTEL NET MANAGER
+echo ================================================
+echo.
+echo Stopping the background auto-login process...
+taskkill /F /IM run.exe /T >nul 2>&1
+
+echo.
+echo Deleting saved credentials...
+del "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\wifi_creds.json" >nul 2>&1
+
+echo.
+echo Done! Your old password has been completely removed.
+echo To enter your new password, just double-click your main Auto-Login app again.
+echo.
+pause
